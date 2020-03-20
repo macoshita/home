7101c9dfa74e4b432fe2a5fbf5508c186936df3a
macoshita <macoshita@gmail.com>
2020-03-20 16:10:15 +0900

    TypeScript, eslint, prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
-import Head from 'next/head'
+import Head from "next/head";
 
-const Home = () => (
+const Home = (): JSX.Element => (
   <div className="container">
     <Head>
       <title>Create Next App</title>
@@ -198,6 +198,6 @@ const Home = () => (
       }
     `}</style>
   </div>
-)
+);
 
-export default Home
+export default Home;
